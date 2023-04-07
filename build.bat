a97cabd538c30f465d1a56c72f4efbf68ce3c32b
Elliot Ayrey <elliot.ayrey@gmail.com>
2023-04-07 17:51:07 +1200

    download the .asm dynamically

--- build.bat
+++ build.bat
@@ -4,9 +4,15 @@ setlocal EnableDelayedExpansion
 
 call "%USERPROFILE%\code\msvc\setup.bat"
 
-call nasm listing_37.asm
-call nasm listing_38.asm
-call nasm listing_39.asm
-call nasm listing_40.asm
+if exist computer_enhance (
+    pushd computer_enhance
+    call git pull
+    popd
+) else (
+    call git clone https://github.com/cmuratori/computer_enhance.git
+)
+robocopy computer_enhance\perfaware\part1 . listing_*.asm > NUL
+
+for %%f in (listing_*.asm) do (call nasm %%f)
 
 cl.exe -nologo main.c
